576bb97a83bdbea95a285243fc30762a06607d43
Gue Jia Xuan (Johnny) <jguejx@gmail.com>
2025-05-28 23:18:49 +0100

    edited readme to fix import issue

--- initial_run.bat
+++ initial_run.bat
@@ -5,7 +5,7 @@ REM Core packages
 pip install pandas geopandas fiona
 
 REM Google API client libraries
-pip install google-api-python-client google-auth google-auth-oauthlib
+pip install google-api-python-client google-auth google-auth-oauthlib openpyxl
 
 echo All packages installed.
 pause
